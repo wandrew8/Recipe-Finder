3d2d90fddb1e304ba5886ffcb69b62a8657b1fb0
Andrew Weiss <wandrew8@gmail.com>
2020-01-10 11:04:27 -0800

    initial react app files added

---
+++ recipe-finder/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="App">
+      <h1>Recipe Finder</h1>
+      <p>New react app that allows users to search through recipes based on their category</p>
+    </div>
+  );
+}
+
+export default App;
