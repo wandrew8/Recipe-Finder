96d32185f2b81dc2918b4835c39773b248965d8f
Andrew Weiss <wandrew8@gmail.com>
2020-01-10 17:12:16 -0800

    props passed as router params

--- recipe-finder/src/App.js
+++ recipe-finder/src/App.js
@@ -1,24 +1,31 @@
 import React , { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import beef from './beef';
-import category from './categories';
 import NavbarComponent from './components/NavbarComponent';
 import HomePage from './components/Homepage';
 import SavedPage from './components/SavedPage';
 import CategoryPage from './components/CategoryPage'
 import Footer from './components/Footer';
+import FOOD from './food';
+import CATEGORIES from './categories';
 import './css/App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      categories: category,
-      beef: beef
+      categories: CATEGORIES,
+      food: FOOD,
     }
   }
   
   render() {
+    const CategoryType = ({ match }) => {
+      return (
+        <CategoryPage 
+        food={this.state.food.filter(foodItem => foodItem.type === match.params.category)} 
+        />
+      )
+    }
 
   return (
     <Router>
@@ -26,7 +33,7 @@ class App extends Component {
       <NavbarComponent categories={this.state.categories} />
       <Switch>
         <Route path="/home" component={HomePage} />
-        <Route path="/category/:category" component={CategoryPage} />
+        <Route path="/category/:category" component={CategoryType} />} />
         <Route exact path="/saved" component={SavedPage} />
         <Redirect to="/home" />
       </Switch>
